a30e4432d375a3d0b2c91c5c8d92a35eab4554d0
Andrii Stelmashenko <>
2019-08-27 15:08:57 +0300

    acceptance tests added, not working

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -15,7 +15,8 @@ let init = co.wrap(function* () {
     process.env.restaurants_table = 'restaurants';
     process.env.AWS_REGION = 'us-east-1';
     process.env.cognito_client_id = 'test_cognito_client_id';
-    process.env.cognito_user_pool_id = 'test_cognito_user_pool_id';
+    process.env.cognito_user_pool_id = 'us-east-1_DufZfQABc';
+    process.env.cognito_server_client_id = 'l5jsi160j8qitb12civ5kpaod';
 
     let cred = (yield awscred.loadAsync()).credentials;
     process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
--- tests/test_cases/get-index.js
+++ tests/test_cases/get-index.js
@@ -16,12 +16,11 @@ describe(`When we invoke the GET / endpoint`, co.wrap(function* () {
     let res = yield when.we_invoke_get_index();
 
     expect(res.statusCode).to.equal(200);
-    // expect(res.headers['Content-Type']).to.equal('text/html; charset=UTF-8');
-    // expect(res.body).to.not.be.null;
+    expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8');
+    expect(res.body).to.not.be.null;
 
-    // const $ = cheerio.load(res.body);
-    // let restaurants = $('.restaurant', '#restaurantsUl');  
-    // expect(restaurants.length).to.equal(8);
-
+    const $ = cheerio.load(res.body);
+    let restaurants = $('.restaurant', '#restaurantsUl');  
+    expect(restaurants.length).to.equal(8);
   }));
 }));
